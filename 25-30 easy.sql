54ff8e86bd51c2c06b5b92b2a9ea4389542b92be
Anas9356 <98730862+Anas9356@users.noreply.github.com>
2026-01-30 22:38:48 +0530

    Update 25-30 easy.sql

--- 25-30 easy.sql
+++ 25-30 easy.sql
@@ -10,3 +10,38 @@ WHERE o.order_date >= '2020-02-01'
   AND o.order_date <  '2020-03-01'
 GROUP BY p.product_name
 HAVING SUM(o.unit) >= 100;
+
+1179. Reformat Department Table
+-- SELECT
+--     id,
+--     SUM(CASE WHEN month = 'Jan' THEN revenue END) AS Jan_Revenue,
+--     SUM(CASE WHEN month = 'Feb' THEN revenue END) AS Feb_Revenue,
+--     SUM(CASE WHEN month = 'Mar' THEN revenue END) AS Mar_Revenue,
+--     SUM(CASE WHEN month = 'Apr' THEN revenue END) AS Apr_Revenue,
+--     SUM(CASE WHEN month = 'May' THEN revenue END) AS May_Revenue,
+--     SUM(CASE WHEN month = 'Jun' THEN revenue END) AS Jun_Revenue,
+--     SUM(CASE WHEN month = 'Jul' THEN revenue END) AS Jul_Revenue,
+--     SUM(CASE WHEN month = 'Aug' THEN revenue END) AS Aug_Revenue,
+--     SUM(CASE WHEN month = 'Sep' THEN revenue END) AS Sep_Revenue,
+--     SUM(CASE WHEN month = 'Oct' THEN revenue END) AS Oct_Revenue,
+--     SUM(CASE WHEN month = 'Nov' THEN revenue END) AS Nov_Revenue,
+--     SUM(CASE WHEN month = 'Dec' THEN revenue END) AS Dec_Revenue
+-- FROM Department
+-- GROUP BY id;
+/* Write your T-SQL query statement below */
+SELECT
+    id,
+    MAX(CASE WHEN month = 'Jan' THEN revenue END) AS Jan_Revenue,
+    MAX(CASE WHEN month = 'Feb' THEN revenue END) AS Feb_Revenue,
+    MAX(CASE WHEN month = 'Mar' THEN revenue END) AS Mar_Revenue,
+    MAX(CASE WHEN month = 'Apr' THEN revenue END) AS Apr_Revenue,
+    MAX(CASE WHEN month = 'May' THEN revenue END) AS May_Revenue,
+    MAX(CASE WHEN month = 'Jun' THEN revenue END) AS Jun_Revenue,
+    MAX(CASE WHEN month = 'Jul' THEN revenue END) AS Jul_Revenue,
+    MAX(CASE WHEN month = 'Aug' THEN revenue END) AS Aug_Revenue,
+    MAX(CASE WHEN month = 'Sep' THEN revenue END) AS Sep_Revenue,
+    MAX(CASE WHEN month = 'Oct' THEN revenue END) AS Oct_Revenue,
+    MAX(CASE WHEN month = 'Nov' THEN revenue END) AS Nov_Revenue,
+    MAX(CASE WHEN month = 'Dec' THEN revenue END) AS Dec_Revenue
+FROM Department
+GROUP BY id
