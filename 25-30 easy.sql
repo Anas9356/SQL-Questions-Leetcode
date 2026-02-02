7b146c1728a6b33823960b9dd2923754d578f69f
Anas9356 <98730862+Anas9356@users.noreply.github.com>
2026-02-02 23:44:42 +0530

    Update 25-30 easy.sql

--- 25-30 easy.sql
+++ 25-30 easy.sql
@@ -72,4 +72,19 @@ GROUP BY sell_date
 ORDER BY sell_date;
 
 
+1211. Queries Quality and Percentage
+SELECT
+    query_name,
+    ROUND(
+        AVG(1.0 * rating / position),
+        2
+    ) AS quality,
+    ROUND(
+        100.0 * SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*),
+        2
+    ) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
+
+
 
